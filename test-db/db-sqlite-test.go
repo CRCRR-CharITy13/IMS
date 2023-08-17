1f5d0c23d042dcfb9f6c6e3f233a9c5ceaf43bf4
trantuantubk <trantuantubk@gmail.com>
2023-08-17 08:22:10 -0400

    test insert transaction

--- test-db/db-sqlite-test.go
+++ test-db/db-sqlite-test.go
@@ -29,7 +29,15 @@ func main() {
 	// retrive items from the Items table
 	var item Item
 	db.First(&item, 1)
-	db.Model(&item).Update("Size", "XXL")
+	// update this item
+	db.Model(&item).Update("Size", "XL")
+	//
+	// add an item
+	newItem := Item{Name: "Men Long T-Shirt", SKU: "M2233MLT", Size: "L", Stock: 100, Price: 5.0}
+	result := db.Create(&newItem)
+	if result.Error != nil {
+		panic("Failed to insert new item to the database")
+	}
 	//
 	var items []Item
 	db.Find(&items)
@@ -38,5 +46,7 @@ func main() {
 	for _, item := range items {
 		fmt.Printf("ID: %d, Name: %s, SKU: %s, Size: %s, Stock: %d, Price: %f\n", item.ID, item.Name, item.SKU, item.Size, item.Stock, item.Price)
 	}
+
+	//
 
 }
