644720477c80ad2945dacb484ca600aa3f5db5ef
trantuantubk <trantuantubk@gmail.com>
2023-08-21 08:27:02 -0400

    automatic import data

--- test-db/data/data-utils.go
+++ test-db/data/data-utils.go
@@ -2,7 +2,9 @@ package data_utils
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
+	"strconv"
 )
 
 type Location struct {
@@ -10,7 +12,67 @@ type Location struct {
 	Description string
 }
 
-func ReadCSV(filename string) ([]Location, error) {
+type Item struct {
+	SKU   string
+	Name  string
+	Stock int
+	Price float32
+	Size  string
+}
+
+type ItemLocation struct {
+	SKU      string
+	Location string
+	Stock    int
+}
+
+func ReadItem(filename string) ([]Item, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var items []Item
+
+	for _, record := range records[1:] {
+
+		stock, err := strconv.Atoi(record[2])
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return nil, err
+		}
+
+		price, err := strconv.ParseFloat(record[3], 32)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return nil, err
+		}
+
+		price32 := float32(price)
+
+		item := Item{
+			SKU:   record[0],
+			Name:  record[1],
+			Stock: stock,
+			Price: price32,
+			Size:  record[4],
+		}
+		items = append(items, item)
+	}
+	return items, err
+}
+
+func ReadLocation(filename string) ([]Location, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
@@ -36,4 +98,40 @@ func ReadCSV(filename string) ([]Location, error) {
 		locations = append(locations, location)
 	}
 	return locations, err
+}
+
+func ReadItemLocation(filename string) ([]ItemLocation, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var itemLocations []ItemLocation
+
+	for _, record := range records[1:] {
+		stock, err := strconv.Atoi(record[2])
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return nil, err
+		}
+
+		itemLocation := ItemLocation{
+			SKU:      record[0],
+			Location: record[1],
+			Stock:    stock,
+		}
+		itemLocations = append(itemLocations, itemLocation)
+	}
+	return itemLocations, err
 }
--- test-db/main.go
+++ test-db/main.go
@@ -3,20 +3,76 @@ package main
 import (
 	"fmt"
 	data_utils "test-db/data/data-utils"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
+
+// Item has and belongs to many locations, `item_locations` is the join table
+type Item struct {
+	gorm.Model
+	SKU   string
+	Name  string
+	Stock int
+	Price float32
+	Size  string
+}
+
+type Location struct {
+	gorm.Model
+	Name        string
+	Description string
+}
+
+type ItemLocation struct {
+	gorm.Model
+	ItemSKU      string
+	LocationName string
+	Stock        int
+}
 
 func main() {
 	fmt.Printf("==== To Test The GIK-IMS Database\n")
-	// itemFileName := "data/gik-ims-items.csv"
+	itemFileName := "data/gik-ims-items.csv"
 	locationFileName := "data/gik-ims-locations.csv"
-	locations, err := data_utils.ReadCSV(locationFileName)
+	itemLocationFileName := "data/gik-ims-items-locations.csv"
+
+	data_items, err := data_utils.ReadItem(itemFileName)
+	data_locations, err := data_utils.ReadLocation(locationFileName)
+	data_itemLocations, err := data_utils.ReadItemLocation(itemLocationFileName)
+
 	if err != nil {
-		fmt.Errorf("Error reading %s: %v\n", locationFileName, err)
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("List of items:\n")
+
+	fmt.Println("Start to connect to the database")
+	// connect to the database
+	db, err := gorm.Open(sqlite.Open("gik-ims-testdb.sqlite"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	db.AutoMigrate(&Item{}, &Location{}, &ItemLocation{})
+
+	for _, item := range data_items {
+		fmt.Printf("%s \t %s \t %d \t %0.2f \t %s \n", item.SKU, item.Name, item.Stock, item.Price, item.Size)
+		item_record := Item{SKU: item.SKU, Name: item.Name, Stock: item.Stock, Price: item.Price, Size: item.Size}
+		db.Create(&item_record)
 	}
 
 	fmt.Println("List of locations:\n")
-	for _, location := range locations {
-		fmt.Printf("%s : %s\n", location.Name, location.Description)
+	for _, location := range data_locations {
+		fmt.Printf("%s \t %s\n", location.Name, location.Description)
+		location_record := Location{Name: location.Name, Description: location.Description}
+		db.Create(&location_record)
 	}
+	fmt.Println("List of item - location :\n")
+	for _, itemLocation := range data_itemLocations {
+		fmt.Printf("%s : \t %s: \t %d\n", itemLocation.SKU, itemLocation.Location, itemLocation.Stock)
+		itemLocation_item := ItemLocation{ItemSKU: itemLocation.SKU, LocationName: itemLocation.Location, Stock: itemLocation.Stock}
+		db.Create(&itemLocation_item)
+	}
+	//
 
 }
