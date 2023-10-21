92be5a9efe4d99fc87e03a4a0121b5da72c04db1
trantuantubk <trantuantubk@gmail.com>
2023-10-21 10:27:25 -0400

    donation - table

--- gik-dashboard/src/components/dashboard/transaction/Donations.tsx
+++ gik-dashboard/src/components/dashboard/transaction/Donations.tsx
@@ -1,7 +1,23 @@
+import { 
+    Table 
+} from "@mantine/core";
 export const DonationManager = () => {
     return (
         <>
         <h3>Donations</h3>
+        <Table>
+            <thead>
+                <tr>
+                    <th>
+                        <th> Time </th>
+                        <th> Signed By </th>
+                        <th> Donator </th>
+                        <th> Total Value </th>
+                        <th> Action </th>
+                    </th>
+                </tr>
+            </thead>
+        </Table>
         </>
     )
 }
