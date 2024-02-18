e7d08cc9221b16b9ea56dc0c43e79878d691132c
mebsim <ebsim.mohamed@gmail.com>
2024-02-17 22:39:49 -0500

    Early 404 page update.

--- gik-dashboard/src/routes/PageNotFound.tsx
+++ gik-dashboard/src/routes/PageNotFound.tsx
@@ -1,12 +1,30 @@
-import { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Container, Title, Text, Button, Group, Space } from '@mantine/core';
+import {Error404} from "tabler-icons-react";
+
+const style = {marginTop: "18%",
+               marginLeft: "25%",
+               marginRight: "25%"
+              }
+
 const PageNotFound = () => {
-    return (
+  return (
+    <div style={style}>
+        <Space h="md" />
+        <Space h="md" />
         <div>
-            <h1>404 - Page Not Found</h1>
-            <Link to="/">Click here to return to the main page </Link>
-            
+          <Title order={1} align="center">404 - Page Not Found</Title>
+          <Space h="md" />
+          <Text c="dimmed" size="lg" ta="center">
+            Page you are trying to open does not exist. You may have mistyped the address, or the
+            page has been moved to another URL. If you think this is an error contact support.
+          </Text>
+          <Space h="md" />
+          <Group position="center">
+            <Button size="md" component="a" href="/">Take me back to home page</Button>
+          </Group>
         </div>
-    );
-};
+    </div>
+  );
+}
+
 export default PageNotFound;
